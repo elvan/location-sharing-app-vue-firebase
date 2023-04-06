526d4ac854bd2629cd941f9b528d13884388412d
Elvan Hidayat <elvanhd@gmail.com>
2023-04-06 14:03:27 +0700

    some final styles

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -92,4 +92,17 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.view-profile .card {
+  padding: 20px;
+  margin-top: 60px;
+}
+.view-profile h2 {
+  font-size: 2em;
+  margin-top: 0;
+}
+.view-profile li {
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+}
+</style>
